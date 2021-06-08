555d1bb1918488bd144b670f37b9b1dac9ebd163
dgl1231 <63222048+dgl1231@users.noreply.github.com>
2021-06-08 19:24:52 +0900

    set tablet model
    set for tablet

--- main.js
+++ main.js
@@ -8,30 +8,25 @@
         toggleElements();
     });
 
+    window.addEventListener('resize', function() {
+        if(window.innerWidth > 1024){
+            offElements();
+
+        }
+    });
+
     function toggleElements() {
         [].forEach.call($toggles, function (toggle) {
             toggle.classList.toggle('on');
         });
     }
 
-    function initMap() {
-        const myLatLng = {
-            lat: 37.782293,
-            lng: -122.391240
-        }
-        const map = new google.maps.Map(
-            document.getElementById('map'), {
-                center: myLatLng,
-                scrollwheel: false,
-                zoom: 18
-            }
-        );
-        const marker = new google.maps.Marker({
-            position: myLatLng,
-            map: map,
-            title: 'GitHub'
+    function offElements() {
+        [].forEach.call($toggles, function (toggle) {
+            toggle.classList.remove('on');
         });
-
     }
+
+    
 
 })(window, document)
